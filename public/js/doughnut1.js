10b38bd88c4908e8c176f69249b6c95438285239
gobind rai <gobind.rai@kochartech.com>
2022-12-15 16:42:17 +0530

    border

--- public/js/doughnut1.js
+++ public/js/doughnut1.js
@@ -3,12 +3,12 @@
 // var express = require('express');
 // var router = express.Router();
 
-var ROOT_PATH = 'https://echarts.apache.org/examples';
+var ROOT_PATH = "https://echarts.apache.org/examples";
 
-var chartDom1 = document.getElementById('main1');
+var chartDom1 = document.getElementById("main1");
 var myChart1 = echarts.init(chartDom1);
 var option1;
-let text1 = '';
+let text1 = "";
 let checkAllData = 0;
 
 // $("#main1").append("<div id='loading1' class='loading';></div");
@@ -20,102 +20,131 @@ let checkAllData = 0;
 // };
 
 // 'http://54.197.121.111:8001/iot/1.6/public/getSiteHealthStatus?business_id=193'
-console.log("url is:  ", document.getElementById('fetchHost').innerHTML + "/getChartData");
+console.log(
+    "url is:  ",
+    document.getElementById("fetchHost").innerHTML + "/getChartData"
+);
 
-console.log("url is check it:  ", document.getElementById('fetchHost').innerHTML + "/getSensorData");
-fetch(document.getElementById('fetchHost').innerHTML + "/getSensorData").then(resSensor => {
-    return resSensor.json();
-})
-    .then(dataSensor => {
+console.log(
+    "url is check it:  ",
+    document.getElementById("fetchHost").innerHTML + "/getSensorData"
+);
+fetch(document.getElementById("fetchHost").innerHTML + "/getSensorData")
+    .then((resSensor) => {
+        return resSensor.json();
+    })
+    .then((dataSensor) => {
         checkAllData++;
-        console.log(dataSensor[0]['count(id)']);
-        sessionStorage.setItem('faultySensorCount', dataSensor[0]['count(id)']);
-        
+        console.log(dataSensor[0]["count(id)"]);
+        sessionStorage.setItem("faultySensorCount", dataSensor[0]["count(id)"]);
+
         if (checkAllData == 2) {
             document.body.style.opacity = 1;
-            document.getElementById('loaderImgBlack').remove();
+            document.getElementById("loaderImgBlack").remove();
             document.body.style.pointerEvents = "auto";
         }
     })
-    .catch(error => console.log('ERROR: ', error));
+    .catch((error) => console.log("ERROR: ", error));
 
-fetch(document.getElementById('fetchHost').innerHTML + "/getChartData").then(res => {
-    return res.json();
-})
-    .then(data => {
-
+fetch(document.getElementById("fetchHost").innerHTML + "/getChartData")
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
         checkAllData++;
 
+        sessionStorage.setItem("nvrOnlineCount", data.nvr[0].online_count);
+        sessionStorage.setItem("nvrOfflineCount", data.nvr[0].offline_count);
 
+        sessionStorage.setItem("glOnlineCount", data.gl[0].online);
+        sessionStorage.setItem("glOfflineCount", data.gl[0].offline);
 
-        sessionStorage.setItem('nvrOnlineCount', data.nvr[0].online_count);
-        sessionStorage.setItem('nvrOfflineCount', data.nvr[0].offline_count);
+        sessionStorage.setItem("inOnlineCount", data.in[0].in_online_count);
+        sessionStorage.setItem("inOfflineCount", data.in[0].in_offline_count);
 
-        sessionStorage.setItem('glOnlineCount', data.gl[0].online);
-        sessionStorage.setItem('glOfflineCount', data.gl[0].offline);
+        sessionStorage.setItem("armCount", data.in[0].arm_count);
+        sessionStorage.setItem("disarmCount", data.in[0].disarm_count);
 
-        sessionStorage.setItem('inOnlineCount', data.in[0].in_online_count);
-        sessionStorage.setItem('inOfflineCount', data.in[0].in_offline_count);
+        sessionStorage.setItem("compCount", data.nvr[0].comp_count);
+        sessionStorage.setItem("nonCompCount", data.nvr[0].non_cpm_count);
 
-        sessionStorage.setItem('armCount', data.in[0].arm_count);
-        sessionStorage.setItem('disarmCount', data.in[0].disarm_count);
+        sessionStorage.setItem("offSites", data.in[0].in_offline_count);
+        sessionStorage.setItem("hardDriveCount", data.nvr[0].hdd_count);
+        sessionStorage.setItem("open-noti", data.openNotification[0].count);
 
-        sessionStorage.setItem('compCount', data.nvr[0].comp_count);
-        sessionStorage.setItem('nonCompCount', data.nvr[0].non_cpm_count);
+        sessionStorage.setItem(
+            "cameraOnlineCount",
+            Number(data.nvr[0].camera_online_count)
+        );
+        sessionStorage.setItem(
+            "cameraOfflineCount",
+            data.nvr[0].camera_offline_count
+        );
 
-        sessionStorage.setItem('offSites', data.in[0].in_offline_count);
-        sessionStorage.setItem('hardDriveCount', data.nvr[0].hdd_count);
-        sessionStorage.setItem('open-noti', data.openNotification[0].count);
+        sessionStorage.setItem("modbusOnlineCount", data.modbus[0].online);
+        sessionStorage.setItem("modbusOfflineCount", data.modbus[0].offline);
 
-        sessionStorage.setItem('cameraOnlineCount', Number(data.nvr[0].camera_online_count));
-        sessionStorage.setItem('cameraOfflineCount', data.nvr[0].camera_offline_count);
+        document.getElementById("inonlinecount").innerHTML =
+            "<span>Online</span> " + sessionStorage.getItem("inOnlineCount");
+        document
+            .getElementById("inonlinecount")
+            .setAttribute("class", "br-right");
+        document.getElementById("inofflinecount").innerHTML =
+            "<span>Offline</span> " + sessionStorage.getItem("inOfflineCount");
 
-        sessionStorage.setItem('modbusOnlineCount', data.modbus[0].online);
-        sessionStorage.setItem('modbusOfflineCount', data.modbus[0].offline);
+        document.getElementById("siteoff").innerHTML =
+            sessionStorage.getItem("offSites");
+        document.getElementById("hddCount").innerHTML =
+            Number(sessionStorage.getItem("nvrOnlineCount")) +
+            Number(sessionStorage.getItem("nvrOfflineCount")) -
+            Number(sessionStorage.getItem("hardDriveCount"));
+        document.getElementById("open-noti").innerHTML =
+            sessionStorage.getItem("open-noti");
+        document.getElementById("faultySensorCount").innerHTML =
+            sessionStorage.getItem("faultySensorCount");
 
-        document.getElementById('inonlinecount').innerHTML = 'Online: ' + sessionStorage.getItem('inOnlineCount');
-        document.getElementById('inofflinecount').innerHTML = 'Offline: ' + sessionStorage.getItem('inOfflineCount');
-
-        document.getElementById('siteoff').innerHTML = sessionStorage.getItem('offSites');
-        document.getElementById('hddCount').innerHTML = Number(sessionStorage.getItem('nvrOnlineCount'))+Number(sessionStorage.getItem('nvrOfflineCount'))-Number(sessionStorage.getItem('hardDriveCount'));
-        document.getElementById('open-noti').innerHTML = sessionStorage.getItem('open-noti');
-        document.getElementById('faultySensorCount').innerHTML = sessionStorage.getItem('faultySensorCount');
-
-        if (sessionStorage.getItem('inOnlineCount') == 'null') {
-            text1 = 'IN not present at site';
+        if (sessionStorage.getItem("inOnlineCount") == "null") {
+            text1 = "IN not present at site";
         }
 
-        let onlinePercent = Math.round((Number(sessionStorage.getItem('inOnlineCount')) / (Number(sessionStorage.getItem('inOnlineCount')) + Number(sessionStorage.getItem('inOfflineCount')))) * 100);
-        let offlinePercent = Math.round((Number(sessionStorage.getItem('inOfflineCount')) / (Number(sessionStorage.getItem('inOnlineCount')) + Number(sessionStorage.getItem('inOfflineCount')))) * 100);
+        let onlinePercent = Math.round(
+            (Number(sessionStorage.getItem("inOnlineCount")) /
+                (Number(sessionStorage.getItem("inOnlineCount")) +
+                    Number(sessionStorage.getItem("inOfflineCount")))) *
+                100
+        );
+        let offlinePercent = Math.round(
+            (Number(sessionStorage.getItem("inOfflineCount")) /
+                (Number(sessionStorage.getItem("inOnlineCount")) +
+                    Number(sessionStorage.getItem("inOfflineCount")))) *
+                100
+        );
 
         option1 = {
             title: {
                 text: text1,
                 // text: 'NVR',
                 // subtext: 'Real-time data',
-                left: 'center'
+                left: "center",
             },
-            height: '80%',
-            width: '100%',
-            color: [
-                '#FF0000',
-                '#0361a1',
-            ],
+            height: "80%",
+            width: "100%",
+            color: ["#FF0000", "#0361a1"],
             // width: '60%',
             series: [
                 {
-                    position: 'auto',
-                    type: 'pie',
-                    radius: ['40%', '70%'],
+                    position: "auto",
+                    type: "pie",
+                    radius: ["40%", "70%"],
                     avoidLabelOverlap: true,
                     itemStyle: {
                         borderRadius: 0,
-                        borderColor: '#fff',
-                        borderWidth: 2
+                        borderColor: "#fff",
+                        borderWidth: 2,
                     },
                     label: {
                         show: true,
-                        position: 'outside',
+                        position: "outside",
                         // distanceToLabelLine: -20,
                         // edgeDistance: '-20%',
                         // bleedMargin: 0,
@@ -126,7 +155,7 @@ fetch(document.getElementById('fetchHost').innerHTML + "/getChartData").then(res
                     },
                     labelLayout: {
                         verticalAlign: "bottom",
-                        align: "left"
+                        align: "left",
                     },
                     labelLine: {
                         show: true,
@@ -135,14 +164,20 @@ fetch(document.getElementById('fetchHost').innerHTML + "/getChartData").then(res
                         label: {
                             show: true,
                             fontSize: "18",
-                            fontWeight: "regular"
-                        }
+                            fontWeight: "regular",
+                        },
                     },
                     data: [
-                        { value: offlinePercent, name: 'offline: ' + offlinePercent + '%' },
-                        { value: onlinePercent, name: 'online: ' + onlinePercent + '%' },
-                    ]
-                }
+                        {
+                            value: offlinePercent,
+                            name: "offline: " + offlinePercent + "%",
+                        },
+                        {
+                            value: onlinePercent,
+                            name: "online: " + onlinePercent + "%",
+                        },
+                    ],
+                },
             ],
             legend: [
                 {
@@ -150,26 +185,27 @@ fetch(document.getElementById('fetchHost').innerHTML + "/getChartData").then(res
                     bottom: 10,
                     orient: "vertical",
                     selectorLabel: {
-                        show: false
+                        show: false,
                     },
-                    left: 'center',
-                    data: ['offline: ' + offlinePercent + '%', 'online: ' + onlinePercent + '%']
-                }
+                    left: "center",
+                    data: [
+                        "offline: " + offlinePercent + "%",
+                        "online: " + onlinePercent + "%",
+                    ],
+                },
             ],
         };
 
         option1 && myChart1.setOption(option1);
         runArmDisarm();
     })
-    .catch(error => console.log('ERROR: ', error));
+    .catch((error) => console.log("ERROR: ", error));
 
-myChart1.on('click', 'legend', function (params) {
+myChart1.on("click", "legend", function (params) {
+    console.log("val: ", params.name);
 
-    console.log('val: ', params.name);
-
-    document.getElementById('type').value = 'nvr';
-    document.getElementById('status').value = params.name;
+    document.getElementById("type").value = "nvr";
+    document.getElementById("status").value = params.name;
     console.log("This is the data of the site:  ", params.name);
-    document.getElementById('siteListForm').submit();
-
+    document.getElementById("siteListForm").submit();
 });
