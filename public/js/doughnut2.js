9afa399a27881ba370e8eedc484e3cdd5ce3b350
gobind rai <gobind.rai@kochartech.com>
2022-12-15 14:27:32 +0530

    update icons in search bar

--- public/js/doughnut2.js
+++ public/js/doughnut2.js
@@ -70,7 +70,7 @@ function runArmDisarm() {
             // subtext: 'Real-time data',
             left: "center",
         },
-        // height: '80%',
+        height: "auto",
         width: "100%",
         color: ["#FF0000", "#0361a1"],
         series: [
