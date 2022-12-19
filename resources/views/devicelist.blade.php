fac9f62326a91f97a0d9b377e753178cb4718582
gobind rai <gobind.rai@kochartech.com>
2022-12-19 16:30:29 +0530

    add new page

--- resources/views/devicelist.blade.php
+++ resources/views/devicelist.blade.php
@@ -1,12 +1,181 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Device List Page... Design Remaining...</h1>
-</body>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
+        />
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+            crossorigin="anonymous"
+        />
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"
+        ></script>
+        <link rel="stylesheet" href="{{ asset('css/design.css') }}" />
+
+        <script
+            src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous"
+        ></script>
+
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+            integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
+
+        <link
+            href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.0/css/toastr.css"
+            rel="stylesheet"
+        />
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.0/js/toastr.js"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/echarts/5.3.3/echarts.min.js"></script>
+        <style>
+            .searchBtn {
+                border: none;
+                box-shadow: none;
+                outline: none;
+                background: transparent;
+            }
+
+            .searchLists {
+                list-style: none;
+                padding-left: 0;
+            }
+
+            .searchLists li {
+                margin-bottom: 8px;
+            }
+
+            .searchLists .search-item {
+                border-bottom: 1px solid #dfdfdf;
+                padding: 6px 0px;
+                display: block;
+                text-decoration: none;
+                color: #212529;
+            }
+        </style>
+    </head>
+    <body>
+        <main>
+            <div class="pb-4"></div>
+        </main>
+        <h1>Device List Page... Design Remaining...</h1>
+
+        <div class="card">
+            <div class="card-body site-status">
+                <div class="d-flex">
+                    <div class="graphsection">
+                        <div
+                            id="main3"
+                            class="d-flex flex-column justify-content-between autoheight"
+                        ></div>
+                        <div class="notificationContainer">
+                            <p
+                                id="glonlinecount"
+                                class="notificationContent br-right"
+                            >
+                                <span>Online</span> ...
+                            </p>
+                            <p id="glofflinecount" class="notificationContent">
+                                <span>Offline</span> ...
+                            </p>
+                        </div>
+                    </div>
+                    <div class="notificationWithGraph ps-1">
+                        <ul class="notificationLists mb-0">
+                            <li>
+                                <div
+                                    class="d-flex w-100 align-items-start flex-nowrap gap-1"
+                                >
+                                    <div class="NotificationDate">
+                                        <span>16</span>
+                                        <p>mins</p>
+                                    </div>
+                                    <div class="listData">
+                                        <h5>Gurgaon CC</h5>
+                                        <p>
+                                            Gurgaon Post Office, Gurgaon, along
+                                            with its address
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div
+                                    class="d-flex w-100 align-items-start flex-nowrap gap-1"
+                                >
+                                    <div class="NotificationDate">
+                                        <span>16</span>
+                                        <p>mins</p>
+                                    </div>
+                                    <div class="listData">
+                                        <h5>Gurgaon CC</h5>
+                                        <p>
+                                            Gurgaon Post Office, Gurgaon, along
+                                            with its address
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div
+                                    class="d-flex w-100 align-items-start flex-nowrap gap-1"
+                                >
+                                    <div class="NotificationDate">
+                                        <span>16</span>
+                                        <p>mins</p>
+                                    </div>
+                                    <div class="listData">
+                                        <h5>Gurgaon CC</h5>
+                                        <p>
+                                            Gurgaon Post Office, Gurgaon, along
+                                            with its address
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div
+                                    class="d-flex w-100 align-items-start flex-nowrap gap-1"
+                                >
+                                    <div class="NotificationDate">
+                                        <span>16</span>
+                                        <p>mins</p>
+                                    </div>
+                                    <div class="listData">
+                                        <h5>Gurgaon CC</h5>
+                                        <p>
+                                            Gurgaon Post Office, Gurgaon, along
+                                            with its address
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li class="text-center pb-0 viewAll">
+                                <a href="#" class="text-dark">All</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
