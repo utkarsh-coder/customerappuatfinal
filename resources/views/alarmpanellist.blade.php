dfac08557300ea78d1e1133913588435c7230527
gobind rai <gobind.rai@kochartech.com>
2022-12-20 18:43:14 +0530

    update

--- resources/views/alarmpanellist.blade.php
+++ resources/views/alarmpanellist.blade.php
@@ -68,24 +68,25 @@
                         
                     </div>
 
-                    <div class="row mb-3">
-                        <div class="col-12">
-                            <label for="" class="position-relative w-100">
-                                <span class="searchIcon"
-                                    ><i class="bi bi-search"></i
-                                ></span>
-                                <input
-                                    type="text"
-                                    name=""
-                                    class="searchBar"
-                                    id=""
-                                />
-                            </label>
-                        </div>
-                    </div>
-                     
+                    
   
                 </div>
+                <nav>
+
+                <section>                
+                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                        <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
+                        <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
+                        <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</button>
+                    </div>
+                    </nav>
+                    <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
+                    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+                    <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+                    </div>
+                </section>
+
             </div>
 
     </main>
