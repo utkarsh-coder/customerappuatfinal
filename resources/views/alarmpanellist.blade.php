7327d64fe37dffe8c9502723470235bb33b8884d
gobind rai <gobind.rai@kochartech.com>
2022-12-21 11:37:09 +0530

    update

--- resources/views/alarmpanellist.blade.php
+++ resources/views/alarmpanellist.blade.php
@@ -101,25 +101,56 @@
                             </div>
                         </div>
                     </li>
+ 
+ <li>
+                        <div class="d-flex w-100 align-items-start flex-nowrap gap-1">
+                            <div class="listData">
+                                <h5>Mothkur,Telangana</h5>
+                                <div class="d-flex gap-2 align-items-start">
+                                <p>Fincare Small Finance Bank Ltd,H No.5-245, First Floor Opp APMC Market Yard,Bhongiri Road, Mothkur,Yadadri Dist,Telangana </p>
+                                <span class="badge bg-danger">Offline</span>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
 
                     <li>
                         <div class="d-flex w-100 align-items-start flex-nowrap gap-1">
                             <div class="listData">
                                 <h5>Mothkur,Telangana</h5>
+                                 <div class="d-flex gap-2 align-items-start">
                                 <p>Fincare Small Finance Bank Ltd,H No.5-245, First Floor Opp APMC Market Yard,Bhongiri Road, Mothkur,Yadadri Dist,Telangana </p>
+                                <span class="badge bg-primary">Online</span>
+                                </div>
+                                
                             </div>
                         </div>
                     </li>
-
+                    <li>
+                        <div class="d-flex w-100 align-items-start flex-nowrap gap-1">
+                            <div class="listData">
+                                <h5>Mothkur,Telangana</h5>
+                                <div class="d-flex gap-2 align-items-start">
+                                <p>Fincare Small Finance Bank Ltd,H No.5-245, First Floor Opp APMC Market Yard,Bhongiri Road, Mothkur,Yadadri Dist,Telangana </p>
+                                <span class="badge bg-danger">Offline</span>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
 
                     <li>
                         <div class="d-flex w-100 align-items-start flex-nowrap gap-1">
                             <div class="listData">
                                 <h5>Mothkur,Telangana</h5>
+                                 <div class="d-flex gap-2 align-items-start">
                                 <p>Fincare Small Finance Bank Ltd,H No.5-245, First Floor Opp APMC Market Yard,Bhongiri Road, Mothkur,Yadadri Dist,Telangana </p>
+                                <span class="badge bg-primary">Online</span>
+                                </div>
+                                
                             </div>
                         </div>
                     </li>
+ 
                     </ul>
                     </div>
                     <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
