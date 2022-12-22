6589feaf6e4dec6cc915ea70d2ba817ee10e087b
utkarsh-coder <utkarshblp2k@gmail.com>
2022-12-22 11:47:43 +0530

    uptime line chart

--- public/js/chartDummy.js
+++ public/js/chartDummy.js
@@ -22,7 +22,7 @@ fetch("https://cc.gizmosmart.io/iot/1.6/public/getUptimeDowntime?type=in&from=20
       const d = new Date(data.data[i].created_at);
       d.setDate(d.getDate() - 1);
       let stringDate = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate();
-      inMap.set(stringDate, inMap.has(stringDate) ? inMap.get(stringDate) + (Number(data.data[i].uptime)/100)*24 : (Number(data.data[i].uptime)/100)*24);
+      inMap.set(stringDate, inMap.has(stringDate) ? inMap.get(stringDate) + (Number(data.data[i].uptime) / 100) * 24 : (Number(data.data[i].uptime) / 100) * 24);
       inCount.set(stringDate, inCount.has(stringDate) ? inCount.get(stringDate) + 1 : 0 + 1);
     }
 
@@ -31,37 +31,100 @@ fetch("https://cc.gizmosmart.io/iot/1.6/public/getUptimeDowntime?type=in&from=20
       console.log("string date: ", stringDate);
       if (inCount.has(stringDate)) {
         keyArray[count] = stringDate;
-        valueArray[count] = (inMap.get(stringDate) / (inCount.get(stringDate) * 24))*100;
+        valueArray[count] = Math.round((inMap.get(stringDate) / (inCount.get(stringDate) * 24)) * 10000) / 100;
       }
       traverseDate.setDate(traverseDate.getDate() + 1);
       count++;
     }
 
-    console.log('keyarray: ',keyArray);
-    console.log('valuearray: ',valueArray);
-    console.log('inmap: ',inMap);
-    console.log('incount: ',inCount);
+    console.log('keyarray: ', keyArray);
+    console.log('valuearray: ', valueArray);
+    console.log('inmap: ', inMap);
+    console.log('incount: ', inCount);
+
+    // optiondummy = {
+    //   xAxis: [{
+    //     type: 'category',
+    //     axisLabel: {
+    //       rotate: 70
+    //     },
+    //     data: keyArray
+    //   }],
+    //   yAxis: {
+    //     type: 'value'
+    //   },
+    //   series: [
+    //     {
+    //       data: valueArray,
+    //       type: 'line'
+    //     }
+    //   ]
+    // };
+
+    // optiondummy && myChartdummy.setOption(optiondummy);
 
     optiondummy = {
-      xAxis: [{
-        type: 'category',
-        axisLabel: {
-          rotate: 70
-        },
-        data: keyArray
-      }],
-      yAxis: {
-        type: 'value'
+      title: {
+        text: 'Stacked Area Chart'
       },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          label: {
+            backgroundColor: '#6a7985'
+          }
+        }
+      },
+      legend: {
+        data: ['Uptime']
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          axisLabel: {
+            rotate: 70
+          },
+          boundaryGap: false,
+          data: keyArray
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
       series: [
         {
-          data: valueArray,
-          type: 'line'
+          name: 'Uptime',
+          type: 'line',
+          stack: 'Total',
+          label: {
+            show: false,
+            position: 'top'
+          },
+          areaStyle: {},
+          emphasis: {
+            focus: 'series'
+          },
+          data: valueArray
         }
       ]
     };
 
     optiondummy && myChartdummy.setOption(optiondummy);
-    
+
+
   })
   .catch(error => console.log('ERROR: ', error));
